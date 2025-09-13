8b17a4d00f0fd6886bb9670523db17b3c1cf44c8
M Ahaduzzaman Chowdhury <ahaduzzaman.chd@gmail.com>
2025-09-13 11:03:38 +0600

    Added Comments in CF_202_Div2_A_CinemaLine.cpp

--- Medium/CF_202_Div2_A_CinemaLine.cpp
+++ Medium/CF_202_Div2_A_CinemaLine.cpp
@@ -1,3 +1,7 @@
+// Codeforces Round 202 Div2 A - Cinema Line
+// Difficulty: 1100 (Medium)
+// Tags: greedy, implementation
+
 #include <iostream>
 #include <unordered_map>
 using namespace std;
@@ -9,34 +13,40 @@ int main() {
 
     for (int i = 0; i < n; i++) {
          int a;
-
-            cin >> a;
-        if ( a==25) {
+         cin >> a;
+        
+         if ( a==25) {
             ma_p[a]++;
-        }
-        else if ( a==50 && ma_p[25] >=1) {
+         }
+             
+         else if ( a==50 && ma_p[25] >=1) {
             ma_p[a]++;
             ma_p[25]--;
-        }
-        else if ( a==100 ) {
+         }
+             
+         else if ( a==100 ) {
             if ( ma_p[50] >= 1 && ma_p[25] >= 1 ) {
                 ma_p[50]--;
                 ma_p[25]--;
             }
-            else if ( ma_p[50] == 0 && ma_p[25] >= 3 ) {
+                
+        else if ( ma_p[50] == 0 && ma_p[25] >= 3 ) {
                   ma_p[25]=ma_p[25]-3;
-            }
-            else {
+           }
+          
+        else {
                 cout << "NO" << endl;
                 return 0;
             }
 
         }
+             
         else {
             cout << "NO" << endl;
             return 0;
         }
-    }
+     }
+    
     cout << "YES" << endl;
     return 0;
 }
