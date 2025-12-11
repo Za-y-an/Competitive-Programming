115853fe0a23be4c6aa1dd7de71e2c6a439a2199
M Ahaduzzaman Chowdhury <ahaduzzaman.chd@gmail.com>
2025-12-12 00:22:05 +0600

    Create  CF_146_Div2_A_Coins.cpp

---
+++ Easy/CF_146_Div2_A_Coins.cpp
@@ -0,0 +1,33 @@
+// Educational Codeforces Round 146 Div2 A - Coins
+// Difficulty: 800 (Easy)
+// Tags: implementation, math
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int t;
+    cin>>t;
+
+    while(t--) {
+
+        long long n,k;
+        cin>>n>>k;
+
+        if (n%2==0) {
+            cout<<"YES"<<endl;
+        }
+
+        else if (k%2 !=0 && k<=n) {
+            cout << "YES" << endl;
+        }
+
+        else {
+            cout << "NO" << endl;
+        }
+
+    }
+
+    return 0;
+}
